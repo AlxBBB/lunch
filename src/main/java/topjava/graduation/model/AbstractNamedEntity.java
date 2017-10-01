59af02af11c09c4b05b52620b51755ed60bddb0b
AlxBBB <apbukreev@gmail.com>
2017-10-01 11:41:55 +1000

    Initial commit

---
+++ src/main/java/topjava/graduation/model/AbstractNamedEntity.java
@@ -0,0 +1,39 @@
+package topjava.graduation.model;
+
+//import org.hibernate.validator.constraints.SafeHtml;
+//import ru.javawebinar.topjava.View;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+
+
+@MappedSuperclass
+public abstract class AbstractNamedEntity extends AbstractBaseEntity {
+
+    @NotBlank
+    @Column(name = "name", nullable = false)
+ //   @SafeHtml(groups = {View.ValidatedRestUI.class})
+    protected String name;
+
+    public AbstractNamedEntity() {
+    }
+
+    protected AbstractNamedEntity(Integer id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Entity %s (%s, '%s')", getClass().getName(), getId(), name);
+    }
+}
---
+++ src/main/java/topjava/graduation/model/HasId.java
@@ -0,0 +1,11 @@
+package topjava.graduation.model;
+
+public interface HasId {
+    Integer getId();
+
+    void setId(Integer id);
+
+    default boolean isNew() {
+        return getId() == null;
+    }
+}
